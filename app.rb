e701e11a74a61526893316f592a863c3cc57445e
Mikołaj Pastuszko <mikolaj.pastuszko@gmail.com>
2013-02-21 22:48:30 +0100

    Sinatra application stub with Rack configuration added

---
+++ app.rb
@@ -0,0 +1,31 @@
+# encoding: UTF-8
+
+require 'sinatra'
+require 'sinatra/sequel'
+require 'rack-flash'
+
+configure do
+  enable :logging
+  enable :sessions
+  set :session_secret, 'cojaplace.secret'
+  use Rack::Flash
+  set :database, 'sqlite://db.db'
+end
+
+#migration "Create users" do
+#  database.create_table :guests do
+#    timestamp :registered_at, :null => false
+#    timestamp :activated_at
+#    text :email, :primary_key => true, :null => false
+#    boolean :workshop1, :default => false, :null => false
+#    boolean :workshop2, :default => false, :null => false
+#    boolean :remind, :default => true, :null => false
+#    text :activation_key, :size => 32, :null => false
+#    boolean :active, :default => false, :null => false
+#  end
+#end
+
+get '/' do
+  haml
+end
+
