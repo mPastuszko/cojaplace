aebc630c91adf2869e8868e4ef9da93a395ad439
Mikołaj Pastuszko <mikolaj.pastuszko@gmail.com>
2013-02-25 20:59:38 +0100

    Poltergeist set as default javascript web driver

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,7 +6,10 @@ require File.join(File.dirname(__FILE__), '..', '..', 'app.rb')
 
 require 'capybara'
 require 'capybara/cucumber'
+require 'capybara/poltergeist'
 require 'rspec'
+
+Capybara.javascript_driver = :poltergeist
 
 Capybara.app = Sinatra::Application
 
