b6f916bf1dfb8aed1e1286c7be0533a08072bae7
Mikołaj Pastuszko <mikolaj.pastuszko@gmail.com>
2013-03-24 01:10:29 +0100

    Order items migration added

---
+++ db/migrations/004_create_order_items.rb
@@ -0,0 +1,10 @@
+# encoding: UTF-8
+
+migration "Create order items" do
+  database.create_table :order_items do
+    text :date, :null => false
+    text :user, :null => false
+    double :price, :null => false
+    text :notes, :null => false, :default => ''
+  end
+end
