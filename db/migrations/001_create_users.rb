48f246325c3ad82c0c925d521c93e6f87282d83c
Mikołaj Pastuszko <mikolaj.pastuszko@gmail.com>
2013-04-29 18:34:43 +0200

    Fixtures removed from migrations

--- db/migrations/001_create_users.rb
+++ db/migrations/001_create_users.rb
@@ -4,17 +4,4 @@ migration "Create users" do
  database.create_table :users do
    text :name, :primary_key => true, :unique => true, :null => false
  end
- ['Daniel',
-  'Grzesiek',
-  'Łukasz',
-  'Maciek',
-  'Marcin J.',
-  'Marcin O.',
-  'Marcin T.',
-  'Maurycy',
-  'Mikołaj',
-  'Piotrek',
-  'Wojtek'].each do |name|
-    database[:users].insert(name: name)
-  end
 end
--- db/migrations/002_create_restaurants.rb
+++ db/migrations/002_create_restaurants.rb
@@ -4,8 +4,4 @@ migration "Create restaurants" do
   database.create_table :restaurants do
     text :name, :primary_key => true, :unique => true, :null => false
   end
-  ['Phuong Dong',
-   'pizzeriaservice.pl'].each do |name|
-     database[:restaurants].insert(name: name)
-   end
 end
